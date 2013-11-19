d9afa4e04a7d04fa73342c4a5bd4c11e2d25ef3f
Josh Rendek <josh@bluescripts.net>
2013-11-19 18:19:07 -0500

    Fixes #2 and adds readme at root and displays IP

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -11,14 +11,26 @@ object Application extends Controller {
 
   def index = Action {
     request =>
-      val ipAddress = request.headers.get("X-Forwarded-For") match {
-        case Some(ip: String) => ip
-        case None => request.remoteAddress
+      val user_agent = request.headers.get("User-Agent").mkString
+      println(user_agent)
+      val ipAddress = ipAddr(request)
+      val browser = user_agent.contains("Firefox") | user_agent.contains("Chrome") | user_agent.contains("Safari")
+      if (browser) {
+        Ok(html.readme.render(ipAddress));
+      } else {
+        Ok(ipAddress)
       }
-      Ok(ipAddress);
   }
 
   def readme() = Action {
-    Ok(html.readme())
+    request =>
+      Ok(html.readme.render(ipAddr(request)))
+  }
+
+  private def ipAddr(request: Request[AnyContent]) = {
+    request.headers.get("X-Forwarded-For") match {
+      case Some(ip: String) => ip
+      case None => request.remoteAddress
+    }
   }
 }
