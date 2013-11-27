a2d50122d73eac39863900c90be8adbba9f6231f
Josh Rendek <josh@bluescripts.net>
2013-11-26 23:27:12 -0500

    initial work on #5 ping api

---
+++ app/controllers/Ping.scala
@@ -0,0 +1,28 @@
+package controllers
+
+import play.api.mvc.{Controller, Action}
+import lib.{RequestHelper, TernaryHelper}
+import scala.sys.process._
+import org.json4s.NoTypeHints
+import org.json4s.native.Serialization
+import org.json4s.native.Serialization.write
+import java.net.InetAddress
+
+object Ping extends Controller with TernaryHelper {
+  implicit val formats = Serialization.formats(NoTypeHints)
+  def ping() = Action {
+    request =>
+      Ok(write(net_ping(RequestHelper.ipAddr(request))))
+  }
+
+  def ping_ip(ip: String) = Action {
+    Ok("empty")
+  }
+
+  private def net_ping(ip: String) = {
+    (Seq("ping -c 4", ip).mkString(" ").!!).split("\n").map {
+      res =>
+        res.replaceAll( """^\s+(?m)""", "")
+    }
+  }
+}
--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 play dist
-scp target/universal/ifcfg-api-1.0-SNAPSHOT.zip root@192.168.5.117:/opt/apps/
-ssh root@192.168.5.117 'kill `ps ax | grep ifcfg | grep -v grep | awk "{ print $1 }"`'
-ssh root@192.168.5.117 "cd /opt/apps; mv ifcfg-api-1.0-SNAPSHOT.zip ifcfg.net/; cd ifcfg.net; unzip -o ifcfg-api-1.0-SNAPSHOT.zip"
-ssh root@192.168.5.117 "cd /opt/apps/ifcfg.net/; nohup ./ifcfg-api-1.0-SNAPSHOT/bin/ifcfg-api -Dhttp.port=9393 -Dhttp.address=0.0.0.0 &" &
+scp target/universal/ifcfg-api-1.0-SNAPSHOT.zip root@192.168.5.172:/opt/apps/
+ssh root@192.168.5.172 'kill `ps ax | grep ifcfg | grep -v grep | awk "{ print $1 }"`'
+ssh root@192.168.5.172 "cd /opt/apps; mv ifcfg-api-1.0-SNAPSHOT.zip ifcfg.net/; cd ifcfg.net; unzip -o ifcfg-api-1.0-SNAPSHOT.zip"
+ssh root@192.168.5.172 "cd /opt/apps/ifcfg.net/; nohup ./ifcfg-api-1.0-SNAPSHOT/bin/ifcfg-api -Dhttp.port=9393 -Dhttp.address=0.0.0.0 &" &
