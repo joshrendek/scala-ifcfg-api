05d83bca95088fa222b21d63aeeeb526141c02e6
Josh Rendek <josh@bluescripts.net>
2013-11-28 23:16:33 -0500

    Work on pings for #5 and sanitizing

--- app/controllers/Ping.scala
+++ app/controllers/Ping.scala
@@ -1,6 +1,6 @@
 package controllers
 
-import play.api.mvc.{Controller, Action}
+import play.api.mvc.{AnyContent, Request, Controller, Action}
 import lib.{RequestHelper, TernaryHelper}
 import scala.sys.process._
 import org.json4s.NoTypeHints
@@ -10,18 +10,30 @@ import java.net.InetAddress
 
 object Ping extends Controller with TernaryHelper {
   implicit val formats = Serialization.formats(NoTypeHints)
+
   def ping() = Action {
     request =>
-      Ok(write(net_ping(RequestHelper.ipAddr(request))))
+      val count = sanitize_count(request)
+      Ok(write(net_ping(RequestHelper.ipAddr(request), count)))
   }
 
   def ping_ip(ip: String) = Action {
-    val ipAddr = InetAddress.getByName(ip).getHostAddress
-    Ok(write(net_ping(ipAddr)))
+    request =>
+      val ipAddr = InetAddress.getByName(ip).getHostAddress
+      val count = sanitize_count(request)
+      Ok(write(net_ping(ipAddr, count)))
   }
 
-  private def net_ping(ip: String) = {
-    (Seq("ping -c 4", ip).mkString(" ").!!).split("\n").map {
+  private def sanitize_count(request: Request[AnyContent]) = {
+    val cTmp = request.getQueryString("count")
+    val c = cTmp.isEmpty ? 4 | cTmp.get.toInt
+    if (c < 1) 1
+    else if (c > 20) 20
+    else c
+  }
+
+  private def net_ping(ip: String, count: Int) = {
+    (Seq("ping -c ", count, ip).mkString(" ").!!).split("\n").map {
       res =>
         res.replaceAll( """^\s+(?m)""", "")
     }
